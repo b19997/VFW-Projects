1d925c819e54516f333a0a194889a06a7f3d28fb
Nathaniel <nathaniel@Nathaniels-MacBook-Pro.local>
2013-09-12 12:38:59 -0400

    made some adjustments

---
+++ main.js
@@ -0,0 +1,31 @@
+function updateSlider(slideAmount) {
+
+var display = document.getElementById("selected");
+
+display.innerHTML=slideAmount;
+};
+/* js Events
+click
+submit
+blur
+change
+*/
+
+// variables
+var  myList= document.getElementById("myList");
+var  fname= document.getElementById("fname");
+var  lname= document.getElementById("lname");
+var  pword= document.getElementById("pword");
+var  mail= document.getElementById("mail");
+var  adss= document.getElementById("adss");
+var  cit= document.getElementById("cit");
+var  ste= document.getElementById("ste");
+var  zip= document.getElementById("zip");
+var  male= document.getElementById("male");
+var  fem= document.getElementById("fem");
+var  slide= document.getElementById("slide");
+var  dateSelected= document.getElementById("dateSelected");
+var  notes= document.getElementById("notes");
+
+
+
