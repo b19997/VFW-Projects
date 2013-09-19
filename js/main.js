584939ccb1d1b7c7ff151aa0b625f49b2fa32ab2
Nathaniel <nathaniel@Nathaniels-MacBook-Pro.local>
2013-09-19 00:01:56 -0400

    well on my way to completing web app 3 with 100% functionality, woots are in order?

--- js/main.js
+++ js/main.js
@@ -29,19 +29,19 @@ function getRadioButton(){
 	}
 }
 
-function controlsData(n){
+function controlData(n){
 	switch(n){
 		case "on":
 		    $("profileForm").style.display = "none";
 		    $("deleteData").style.display = "inline";
-		    $("viewData").style.display = "inline";
-		    $("newProfile").style.display = "inline";
+		    $("display").style.display = "none";
+		    $("new").style.display = "inline";
 		    break;
 		case "off":
 		    $("profileForm").style.display = "block";
 		    $("deleteData").style.display = "inline";
-		    $("displayLink").style.display = "inline";
-		    $("newProfile").style.display = "none";
+		    $("display").style.display = "inline";
+		    $("new").style.display = "none";
 		    $("items").style.display = "none";
 		    break;
 		default:
@@ -84,6 +84,7 @@ function previewData(){
 	$("items").style.display = "display";
 	for(var i=0, len=localStorage.length; i<len;i++){
 		var makeLi = document.createElement("li");
+		var moreOptionsLi = document.createElement("li");
 		makeList.appendChild(makeLi);
 		var key = localStorage.key(i);
 		var value = localStorage.getItem(key);
@@ -95,8 +96,58 @@ function previewData(){
 			makeSubList.appendChild(makeSubLi);
 			var optSubText = object[n][0]+" "+object[n][1];
 			makeSubLi.innerHTML = optSubText;
+			makeSubList.appendChild(moreOptionsLi);
 		}
+		makeMoreOptions(localStorage.key(i), moreOptionsLi);
 	}
+}
+function makeMoreOptions(key, moreOptionsLi){
+	var editOptions = document.createElement("a");
+	editOptions.href = "#";
+	editOptions.key = key;
+	var editData = "Edit Profile";
+	editOptions.addEventListener("click", editMoreOptions);
+	editOptions.innerHTML = editData;
+	moreOptionsLi.appendChild(editOptions);
+
+	var breaks = document.createElement("Br");
+	moreOptionsLi.appendChild(breaks);
+
+	var deleteOptions = document.createElement("a");
+	deleteOptions.href = "#";
+	deleteOptions.key = key;
+	var deleteUserText = "Delete Profile";
+	// deleteOptions.addEventListener("click", deleteMoreOptions);
+	deleteOptions.innerHTML = deleteUserText;
+	moreOptionsLi.appendChild(deleteOptions);
+
+}
+function editMoreOptions(){
+       var inputValues = localStorage.getItem(this.key);
+       var item = JSON.parse(inputValues);
+
+       controlData("off");
+
+       $("groups").value = item.group[1];
+       $("fname").value = item.fname[1];
+       $("lname").value = item.lname[1];
+       $("pword").value = item.pword[1];
+       $("mail").value  = item.mail[1];
+       $("adss").value = item.adss [1];
+       $("adds").value = item.adds[1];
+       $("cit").value  = item.cit[1];      
+       $("ste").value  = item.ste[1];
+       $("zip").value  = item.zip[1];
+       var radioButtons = document.forms[0].gender;
+       for(var i=0; i<radioButtons.length; i++){
+       	   if(radioButtons[i].value == "Male" && item.gender[1] == "Male"){
+       	   	}else if(radioButtons[i].value == "Female" && item.gender[1] == "Female"){
+             radioButtons[i].setAttribute("checked", "checked");       	  
+       	    }
+      
+       }
+       $("slide").value = item.slide[1];
+       $("dateSelected").value = item.dateSelected[1];
 }
 
 function clearData(){
